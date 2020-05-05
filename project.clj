c99307c05c772cad3b4307884cad3c19be37e498
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-05-05 14:11:44 -0500

    ci fixed??

--- project.clj
+++ project.clj
@@ -2,7 +2,14 @@
   :description "A Leiningen plugin for the PinkGorilla Notebook."
   :url "https://github.com/pink-gorilla/lein-pinkgorilla"
   :license {:name "MIT"}
+  :description "Clojure<->R interop"
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/release_username
+                                     :password :env/release_password
+                                     :sign-releases false}]]
+
   :dependencies [[org.pinkgorilla/gorilla-notebook "0.4.17"]]
+  
   :eval-in-leiningen true
 
   :min-lein-version "2.9.1"
@@ -17,6 +24,5 @@
                   ["vcs" "commit" "Begin %s"]
                   ["vcs" "push"]]
   :aliases {"bump-version"
-            ["change" "version" "leiningen.release/bump-version"]}
-  )
+            ["change" "version" "leiningen.release/bump-version"]})
 
