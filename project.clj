1be133e09cb471b1476eb81ebc9e14e4a745695e
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-05-05 14:00:38 -0500

    fixed? ci

--- project.clj
+++ project.clj
@@ -3,5 +3,20 @@
   :url "https://github.com/pink-gorilla/lein-pinkgorilla"
   :license {:name "MIT"}
   :dependencies [[org.pinkgorilla/gorilla-notebook "0.4.17"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :min-lein-version "2.9.1"
+  :min-java-version "1.11"
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["bump-version" "release"]
+                  ["vcs" "commit" "Release %s"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["deploy"]
+                  ["bump-version"]
+                  ["vcs" "commit" "Begin %s"]
+                  ["vcs" "push"]]
+  :aliases {"bump-version"
+            ["change" "version" "leiningen.release/bump-version"]}
+  )
 
