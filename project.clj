dc113e9ffe89465eca15592b97497c8d79d694d0
CI/CD <ci@pinkgorilla.org>
2020-05-05 21:55:55 +0000

    Release 0.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/lein-pinkgorilla "0.0.5-SNAPSHOT"
+(defproject org.pinkgorilla/lein-pinkgorilla "0.0.5"
   :description "A Leiningen plugin for the PinkGorilla Notebook."
   :url "https://github.com/pink-gorilla/lein-pinkgorilla"
   :license {:name "MIT"}
